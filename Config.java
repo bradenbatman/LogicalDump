930fd1f464f7955a3c79b8404d0cff5d6006ee6c
bradenbatman <56446173+bradenbatman@users.noreply.github.com>
2019-11-25 12:40:07 -0500

    Add files via upload

---
+++ Config.java
@@ -0,0 +1,41 @@
+//Braden Batman
+//11-25-2019
+//Operating Systems
+//Logical Dump
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+final class Config
+{
+    private static final String filePath = "./parameters.cfg";
+    private static Properties configFile;
+
+    //instance variable allows for only one instance of this class.
+    private static Config instance = null;
+
+    private Config()
+    {
+        configFile = new Properties();
+
+        try {
+            FileInputStream fis = new FileInputStream(filePath);
+            configFile.load(new InputStreamReader(fis));
+            System.out.println("Loaded parameters.cfg...");
+        } catch (IOException e) {
+            System.out.println("ERROR: parameters.cfg was not found, ending program.");
+            System.exit(0);
+            //e.printStackTrace();
+        }
+    }
+    public static String getProperty(String key) throws NullPointerException
+    {
+        //ensures that only one instance of this class can exist.
+        if (instance==null){
+            instance = new Config();
+        }
+        return configFile.getProperty(key);
+    }
+}
